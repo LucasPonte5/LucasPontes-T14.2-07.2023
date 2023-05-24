a0330c05c6b2739d44a5e0f023ee3f869edfe691
LucasPonte5 <lucaspontesdiniz@gmail.com>
2023-05-24 19:37:01 -0300

    Teste

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -9,26 +9,31 @@ from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
-
+    
     def update(self, game):
-        figure = random.randint(0,2)
         if len(self.obstacles) == 0:
-            if figure == 0:
-                self.obstacles.append(Cactus(LARGE_CACTUS[random.randint(0, 2)]))
-            elif figure == 1:
-                self.obstacles.append(Cactus(SMALL_CACTUS[random.randint(0, 2)]))
-            elif figure == 2:
-                self.obstacles.append(Bird(BIRD[random.randint(0,1)]) )
-
+            figura = random.randint(1,3)
+            if figura == 1:
+                self.obstacles.append(Cactus(SMALL_CACTUS[random.randint(0,2)]))
+            elif figura == 2:
+                cactus = Cactus(LARGE_CACTUS[random.randint(0,2)])
+                cactus.rect.y = 300# mudando a posição do cactus Largo
+                self.obstacles.append(cactus)
+            elif figura == 3:
+                self.obstacles.append(Bird(BIRD))
+                
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
             
-
+        
     def draw(self,screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
-        
+            
+    def reset_obstacles(self):
+        self.obstacles.clear()
